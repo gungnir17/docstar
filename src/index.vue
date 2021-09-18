e0d1190ce446329a4233f7c018c4ea584a76f300
gungnir17 <gungnir17@163.com>
2021-09-18 10:30:06 +0800

    Create index.vue

---
+++ src/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello!欢迎来到博心星球"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/docstar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
